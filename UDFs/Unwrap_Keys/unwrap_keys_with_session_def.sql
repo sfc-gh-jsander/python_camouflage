9c60dcfaa024009c0920f6abb935a413722bbf53
Kevin Keller <89779395+sfc-gh-kkeller@users.noreply.github.com>
2022-11-22 15:55:37 +0100

    Delete UDFs/Unwrap_Keys directory

--- UDFs/Unwrap_Keys/unwrap_keys_with_session_def.sql
+++
@@ -1,7 +0,0 @@
-create or replace function decrypt_format_keys_test2(key string, userkeys string, token binary, jtwks binary, currentuser string, currentsession string, pubsessionkey string)
-returns string
-language python
-runtime_version = 3.8
-packages = ('pycryptodome','pyjwt','cryptography')
-IMPORTS = ('@python_libs/unwrap_keys.py')
-handler = 'unwrap_keys.udf'
