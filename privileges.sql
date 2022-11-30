7b6c89087100a408e9ab053143a38d22dcca95fb
Bart van Delft <78418250+bvdsf@users.noreply.github.com>
2022-11-30 14:29:01 +0100

    Update privileges.sql

--- privileges.sql
+++ privileges.sql
@@ -1,104 +1,104 @@
 
 
 grant all privileges on function encrypt_ff3_string(string, string, string) to role ff3_encrypt;
-grant all privileges on function encrypt_ff3_string(string, string, string) to role masked;
+grant all privileges on function encrypt_ff3_string(string, string, string) to role ff3_masked;
 grant all privileges on function decrypt_ff3_string(string, string, string) to role ff3_decrypt;
-grant all privileges on function decrypt_ff3_string(string, string, string) to role data_sc;
-grant all privileges on function decrypt_ff3_string(string, string, string) to role masked;
+grant all privileges on function decrypt_ff3_string(string, string, string) to role ff3_data_sc;
+grant all privileges on function decrypt_ff3_string(string, string, string) to role ff3_masked;
 
-grant all privileges on function format_ff3_string(string) to role data_sc;
-grant all privileges on function sqljoin_ff3_string(string) to role data_sc;
-grant all privileges on function format_email_ff3_string(string) to role data_sc;
+grant all privileges on function format_ff3_string(string) to role ff3_data_sc;
+grant all privileges on function sqljoin_ff3_string(string) to role ff3_data_sc;
+grant all privileges on function format_email_ff3_string(string) to role ff3_data_sc;
 
-grant all privileges on function format_ff3_string(string) to role masked;
-grant all privileges on function sqljoin_ff3_string(string) to role masked;
-grant all privileges on function format_email_ff3_string(string) to role masked;
+grant all privileges on function format_ff3_string(string) to role ff3_masked;
+grant all privileges on function sqljoin_ff3_string(string) to role ff3_masked;
+grant all privileges on function format_email_ff3_string(string) to role ff3_masked;
 
 grant all privileges on function format_ff3_string(string) to role ff3_decrypt;
 grant all privileges on function sqljoin_ff3_string(string) to role ff3_decrypt;
 grant all privileges on function format_email_ff3_string(string) to role ff3_decrypt;
 
-grant all privileges on function format_ff3_string(string) to role masked;
-grant all privileges on function sqljoin_ff3_string(string) to role masked;
-grant all privileges on function format_email_ff3_string(string) to role masked;
+grant all privileges on function format_ff3_string(string) to role ff3_masked;
+grant all privileges on function sqljoin_ff3_string(string) to role ff3_masked;
+grant all privileges on function format_email_ff3_string(string) to role ff3_masked;
 
 grant all privileges on function encrypt_ff3_float(string, float, string) to role ff3_encrypt;
 grant all privileges on function decrypt_ff3_float(string, float, string) to role ff3_decrypt;
-grant all privileges on function decrypt_ff3_float(string, float, string) to role data_sc;
+grant all privileges on function decrypt_ff3_float(string, float, string) to role ff3_data_sc;
 
-grant all privileges on function encrypt_ff3_float(string, float, string) to role masked;
-grant all privileges on function decrypt_ff3_float(string, float, string) to role masked;
-grant all privileges on function decrypt_ff3_float(string, float, string) to role masked;
+grant all privileges on function encrypt_ff3_float(string, float, string) to role ff3_masked;
+grant all privileges on function decrypt_ff3_float(string, float, string) to role ff3_masked;
+grant all privileges on function decrypt_ff3_float(string, float, string) to role ff3_masked;
 
-grant all privileges on function format_ff3_float(float) to role data_sc;
-grant all privileges on function sqljoin_ff3_float(float) to role data_sc;
+grant all privileges on function format_ff3_float(float) to role ff3_data_sc;
+grant all privileges on function sqljoin_ff3_float(float) to role ff3_data_sc;
 
-grant all privileges on function format_ff3_float(float) to role masked;
-grant all privileges on function sqljoin_ff3_float(float) to role masked;
+grant all privileges on function format_ff3_float(float) to role ff3_masked;
+grant all privileges on function sqljoin_ff3_float(float) to role ff3_masked;
 
 grant all privileges on function format_ff3_float(float) to role ff3_decrypt;
 grant all privileges on function sqljoin_ff3_float(float) to role ff3_decrypt;
 
-grant all privileges on function format_ff3_float(float) to role masked;
-grant all privileges on function sqljoin_ff3_float(float) to role masked;
+grant all privileges on function format_ff3_float(float) to role ff3_masked;
+grant all privileges on function sqljoin_ff3_float(float) to role ff3_masked;
 
 grant all privileges on function encrypt_ff3_number_decimal38_8(string, number, string) to role ff3_encrypt;
 grant all privileges on function decrypt_ff3_number_decimal38_8(string, number, string) to role ff3_decrypt;
-grant all privileges on function decrypt_ff3_number_decimal38_8(string, number, string) to role data_sc;
+grant all privileges on function decrypt_ff3_number_decimal38_8(string, number, string) to role ff3_data_sc;
 
-grant all privileges on function encrypt_ff3_number_decimal38_8(string, number, string) to role masked;
-grant all privileges on function decrypt_ff3_number_decimal38_8(string, number, string) to role masked;
+grant all privileges on function encrypt_ff3_number_decimal38_8(string, number, string) to role ff3_masked;
+grant all privileges on function decrypt_ff3_number_decimal38_8(string, number, string) to role ff3_masked;
 
 
 grant all privileges on function format_ff3_number_decimal38_8(number) to role ff3_decrypt;
 grant all privileges on function sqljoin_ff3_number_decimal38_8(number) to role ff3_decrypt;
 
-grant all privileges on function format_ff3_number_decimal38_8(number) to role masked;
-grant all privileges on function sqljoin_ff3_number_decimal38_8(number) to role masked;
+grant all privileges on function format_ff3_number_decimal38_8(number) to role ff3_masked;
+grant all privileges on function sqljoin_ff3_number_decimal38_8(number) to role ff3_masked;
 
-grant all privileges on function format_ff3_number_decimal38_8(number) to role data_sc;
-grant all privileges on function sqljoin_ff3_number_decimal38_8(number) to role data_sc;
+grant all privileges on function format_ff3_number_decimal38_8(number) to role ff3_data_sc;
+grant all privileges on function sqljoin_ff3_number_decimal38_8(number) to role ff3_data_sc;
 
-grant all privileges on function format_ff3_number_decimal38_8(number) to role masked;
-grant all privileges on function sqljoin_ff3_number_decimal38_8(number) to role masked;
+grant all privileges on function format_ff3_number_decimal38_8(number) to role ff3_masked;
+grant all privileges on function sqljoin_ff3_number_decimal38_8(number) to role ff3_masked;
 
 
 grant all privileges on function encrypt_ff3_number_integer(string, number, string) to role ff3_encrypt;
 grant all privileges on function decrypt_ff3_number_integer(string, number, string) to role ff3_decrypt;
-grant all privileges on function decrypt_ff3_number_integer(string, number, string) to role data_sc;
+grant all privileges on function decrypt_ff3_number_integer(string, number, string) to role ff3_data_sc;
 
-grant all privileges on function encrypt_ff3_number_integer(string, number, string) to role masked;
-grant all privileges on function decrypt_ff3_number_integer(string, number, string) to role masked;
+grant all privileges on function encrypt_ff3_number_integer(string, number, string) to role ff3_masked;
+grant all privileges on function decrypt_ff3_number_integer(string, number, string) to role ff3_masked;
 
-grant all privileges on function partial_decrypt_ff3_number_1d_decimal_38_8(string, number, string) to role masked;
+grant all privileges on function partial_decrypt_ff3_number_1d_decimal_38_8(string, number, string) to role ff3_masked;
 grant all privileges on function partial_decrypt_ff3_number_1d_decimal_38_8(string, number, string) to role ff3_decrypt;
-grant all privileges on function partial_decrypt_ff3_number_1d_decimal_38_8(string, number, string) to role data_sc;
+grant all privileges on function partial_decrypt_ff3_number_1d_decimal_38_8(string, number, string) to role ff3_data_sc;
 
-grant all privileges on function partial_decrypt_ff3_number_1d_integer(string, number, string) to role masked;
+grant all privileges on function partial_decrypt_ff3_number_1d_integer(string, number, string) to role ff3_masked;
 grant all privileges on function partial_decrypt_ff3_number_1d_integer(string, number, string) to role ff3_decrypt;
-grant all privileges on function partial_decrypt_ff3_number_1d_integer(string, number, string) to role data_sc;
+grant all privileges on function partial_decrypt_ff3_number_1d_integer(string, number, string) to role ff3_data_sc;
 
 
 
 grant all privileges on function format_ff3_number_integer(number) to role ff3_decrypt;
 grant all privileges on function sqljoin_ff3_number_integer(number) to role ff3_decrypt;
-grant all privileges on function format_ff3_number_integer(number) to role masked;
+grant all privileges on function format_ff3_number_integer(number) to role ff3_masked;
 
 
-grant all privileges on function format_ff3_number_partial_decimal38_8(number(38,8),integer) to role masked;
-grant all privileges on function format_ff3_number_partial_integer(number(38,0),integer) to role masked;
+grant all privileges on function format_ff3_number_partial_decimal38_8(number(38,8),integer) to role ff3_masked;
+grant all privileges on function format_ff3_number_partial_integer(number(38,0),integer) to role ff3_masked;
 
-grant all privileges on function format_ff3_number_partial_decimal38_8(number(38,8),integer) to role data_sc;
-grant all privileges on function format_ff3_number_partial_integer(number(38,0),integer) to role data_sc;
+grant all privileges on function format_ff3_number_partial_decimal38_8(number(38,8),integer) to role ff3_data_sc;
+grant all privileges on function format_ff3_number_partial_integer(number(38,0),integer) to role ff3_data_sc;
 
 grant all privileges on function format_ff3_number_partial_decimal38_8(number(38,8),integer) to role ff3_decrypt;
 grant all privileges on function format_ff3_number_partial_integer(number(38,0),integer)to role ff3_decrypt;
 
-grant all privileges on function format_ff3_number_integer(number) to role data_sc;
-grant all privileges on function sqljoin_ff3_number_integer(number) to role data_sc;
+grant all privileges on function format_ff3_number_integer(number) to role ff3_data_sc;
+grant all privileges on function sqljoin_ff3_number_integer(number) to role ff3_data_sc;
 
-grant all privileges on function format_ff3_number_integer(number) to role masked;
-grant all privileges on function sqljoin_ff3_number_integer(number) to role masked;
+grant all privileges on function format_ff3_number_integer(number) to role ff3_masked;
+grant all privileges on function sqljoin_ff3_number_integer(number) to role ff3_masked;
 
 
 
@@ -108,12 +108,12 @@ grant all privileges on function sqljoin_ff3_number_integer(number) to role mask
 
 grant all privileges on function format_ff3_string_uspostal(string) to role ff3_encrypt;
 grant all privileges on function format_ff3_string_uspostal(string) to role ff3_decrypt;
-grant all privileges on function format_ff3_string_uspostal(string) to role masked;
-grant all privileges on function format_ff3_string_uspostal(string) to role data_sc;
+grant all privileges on function format_ff3_string_uspostal(string) to role ff3_masked;
+grant all privileges on function format_ff3_string_uspostal(string) to role ff3_data_sc;
 
 grant all privileges on function format_ff3_string_usphone(string) to role ff3_encrypt;
 grant all privileges on function format_ff3_string_usphone(string) to role ff3_decrypt;
-grant all privileges on function format_ff3_string_usphone(string) to role masked;
-grant all privileges on function format_ff3_string_usphone(string) to role data_sc;
+grant all privileges on function format_ff3_string_usphone(string) to role ff3_masked;
+grant all privileges on function format_ff3_string_usphone(string) to role ff3_data_sc;
 
 
