bc346b691418aedb30c777fea8a016d3f938d61c
Kevin Keller <kevin.keller@snowflake,com>
2022-12-03 17:27:26 +0100

    first version of working data types testing and demo script

--- Demo_Scripts/datatypes_test.sql
+++ Demo_Scripts/datatypes_test.sql
@@ -1,3 +1,19 @@
+-- This is a demo script to test and showcase how ff3-1 encryption / decryption can be applied to data types 
+-- other than just strings and integers, but also to decimals (in this example number (38,8) ) and that decimals
+-- and integers can even use the same decimal masking policy. Integers are compatible with decimals, but will be converted 
+-- to decimals if the same masking polciy is applied such as the case is here. 
+-- It is also possible to apply a masking policy that only encrypts/decrypts integers to an integer column and 
+-- another masking policy to the decimal column(s) with the appropiate number format if desired. 
+-- The data type float can also be encrypted / decrypted and also has its own set of masking policies and encrypt/decrypt UDFs.
+
+
+use role ff3_standard;
+
+use database ff3_test_db;
+use schema ff3_test_schema;
+use warehouse ff3_test_wh;
+
+
 CREATE or replace TABLE ff3_datatypes_source1 (
   name varchar(255) default NULL,
   phone varchar(100) default NULL,
@@ -35,51 +51,75 @@ set encryptkey='KEY678901';
 create or replace view ff3_datatypes_view1 as  select  $encryptkey as KEYID, * from ff3_datatypes_source1 ;
 
 
--- Grant access rights to view
 
-grant all privileges on view ff3_datatypes_view1 to role FF3_STANDARD;
-grant all privileges on view ff3_datatypes_view1 to role FF3_DECRYPT;
-grant all privileges on view ff3_datatypes_view1 to role FF3_ENCRYPT;
 
-alter view ff3_datatypes_view1 modify column name set tag ff3_encrypt='',column phone set tag ff3_encrypt='',column email set tag ff3_encrypt='',column postalzip set tag ff3_encrypt='',column integernumber set tag ff3_encrypt='',column floatnumber set tag ff3_encrypt='',column decimalnumber set tag ff3_encrypt='';
-
---alter view ff3_datatypes_view1 modify column name unset tag ff3_encrypt,column phone unset tag ff3_encrypt,column email unset tag ff3_encrypt,column postalzip unset tag ff3_encrypt,column integernumber unset tag ff3_encrypt,column floatnumber unset tag ff3_encrypt,column decimalnumber unset tag ff3_encrypt;
 
 use role ff3_tag_admin;
 
-alter tag ff3_encrypt set
-  masking policy ff3_encrypt_string_pass5,
-  masking policy ff3_encrypt_number_pass5_decimal,
-  masking policy ff3_encrypt_float_pass5;
+create or replace tag ff3_encrypt_datatypes;
+create or replace tag ff3_data_sc_decrypt_datatypes;
+
+alter view ff3_datatypes_view1 modify column name set tag ff3_encrypt_datatypes='',column phone set tag ff3_encrypt_datatypes='',column email set tag ff3_encrypt_datatypes='',column postalzip set tag ff3_encrypt_datatypes='',column integernumber set tag ff3_encrypt_datatypes='',column floatnumber set tag ff3_encrypt_datatypes='',column decimalnumber set tag ff3_encrypt_datatypes='';
+
+
+alter tag ff3_encrypt_datatypes set
+  masking policy ENCRYPT_STRING_FF3,
+  masking policy ENCRYPT_NUMBER_FF3_DECIMAL_38_8,
+  masking policy ENCRYPT_FLOAT_FF3;
+  
+use role ff3_standard; 
+
+
+-- Grant access rights to view
+
+grant usage on database ff3_test_db to role ff3_encrypt;
+grant usage on schema ff3_test_schema to role ff3_encrypt;
+
+grant usage on database ff3_test_db to role ff3_decrypt;
+grant usage on schema ff3_test_schema to role ff3_decrypt;
+
+grant usage on database ff3_test_db to role ff3_data_sc;
+grant usage on schema ff3_test_schema to role ff3_data_sc;
+
+grant all privileges on view ff3_datatypes_view1 to role FF3_DATA_SC;
+grant all privileges on view ff3_datatypes_view1 to role FF3_DECRYPT;
+grant all privileges on view ff3_datatypes_view1 to role FF3_ENCRYPT;
+grant all privileges on table ff3_datatypes_target1 to role FF3_DATA_SC;
+grant all privileges on table ff3_datatypes_target1 to role FF3_DECRYPT;
+grant all privileges on table ff3_datatypes_target1 to role FF3_ENCRYPT;
+
+  
 
 use role ff3_encrypt;
+
+set userkeys='''{
+    "678901": ["7d1b1f5d48fed50a53c6c7afffc1b4ec3fc2865a97744b263e285676bc96c055", "CBD09280979564", "56854"],
+    "678902": ["c2051e1a93c3fd7f0e4f20b4fb4f7889aeb8d6fd10f68551af659323f42961e9", "7036604882667B", "85567"]
+}''';
+
 -- Populate target table with encrypted data (stay with role FF3_Encrypt)
  INSERT INTO ff3_datatypes_target1  select * from ff3_datatypes_view1; 
 
+use role ff3_standard;
+
+select * from ff3_datatypes_target1;
 
 
+use role ff3_tag_admin;
 
- ----XXXXXX WORK HERE ---
-
-  grant all privileges on table ff3_pass3_target1 to role accountadmin;
-  grant all privileges on table ff3_pass3_target1 to role sysadmin;
- 
- grant all privileges on view ff3_pass3_target1 to role ff3_encrypt;
-
-grant all privileges on view ff3_pass3_target1 to role ff3_decrypt;
-grant all privileges on view ff3_pass3_target1 to role data_sc;
+alter tag ff3_data_sc_decrypt_datatypes set
+  masking policy DECRYPT_FORMAT_FF3_STRING,
+  masking policy DECRYPT_FORMAT_FF3_DECIMAL_38_8,
+  masking policy DECRYPT_FORMAT_FF3_FLOAT ;
 
 
-grant all privileges on table ff3_pass3_target1 to role ff3_decrypt;
+alter table ff3_datatypes_target1 modify column name set tag ff3_data_sc_decrypt_datatypes='',column phone set tag ff3_data_sc_decrypt_datatypes='',column email set tag ff3_data_sc_decrypt_datatypes='',column postalzip set tag ff3_data_sc_decrypt_datatypes='',column integernumber set tag ff3_data_sc_decrypt_datatypes='',column floatnumber set tag ff3_data_sc_decrypt_datatypes='',column decimalnumber set tag ff3_data_sc_decrypt_datatypes='';
 
-alter tag ff3_data_sc set
-  masking policy ff3_decrypt_format_float_pass5,
-  masking policy ff3_decrypt_format_string_pass5,
-  masking policy ff3_decrypt_format_pass5_decimal ;
+use role ff3_decrypt;
 
--- Change back to Accountadmin
--- Assign decrypt tag to decrypt demo table
+select * from ff3_datatypes_target1;
 
-use role accountadmin;
+use role ff3_data_sc;
 
-alter table ff3_pass3_target1 modify column name set tag ff3_data_sc='',column phone set tag ff3_data_sc='',column email set tag ff3_data_sc='',column postalzip set tag ff3_data_sc='',column integernumber set tag ff3_data_sc='',column floatnumber set tag ff3_data_sc='',column decimalnumber set tag ff3_data_sc='';
+select * from ff3_datatypes_target1;
+
